4db28aeb29a2d95e7b215ef8bd0017026a2c8b4e
itsjibel <itsjibel@gmail.com>
2024-12-17 19:39:46 +0330

    Estimate the abdomen circumference

--- estimate-body-fat.py
+++ estimate-body-fat.py
@@ -3,6 +3,8 @@ from Pixel_Size_Estimation_In_cm import estimate_pixel_size as PixelSizeEstimaio
 from Body_Part_Segmentation import body_parts_segmentation as BodyPartSegmentation
 from Parametric_Body_Fat_Estimation.src import parametric_body_fat_estimation as ParametricBodyFatEstimation
 import math
+import cv2
+import mediapipe as mp
 print('Done!')
 
 HEIGHT = 190
@@ -52,7 +54,7 @@ for y in range(frontal_image_segments.shape[0]):
             neck_bottom_right_y = y
             break
 
-neck_frontal_radius_in_pixel = math.sqrt((neck_bottom_left_x - neck_bottom_right_x) * (neck_bottom_left_x - neck_bottom_right_x) + (neck_bottom_left_y - neck_bottom_right_y) * (neck_bottom_left_y - neck_bottom_right_y))
+neck_frontal_radius_in_pixel = math.sqrt((neck_bottom_left_x - neck_bottom_right_x) * (neck_bottom_left_x - neck_bottom_right_x) + (neck_bottom_left_y - neck_bottom_right_y) * (neck_bottom_left_y - neck_bottom_right_y)) / 2
 neck_frontal_radius_in_cm = neck_frontal_radius_in_pixel * frontal_image_pixel_size[0]
 
 neck_bottom_left_x = 0
@@ -82,18 +84,81 @@ for y in range(left_image_segments.shape[0]):
             neck_bottom_right_y = y
             break
 
-neck_side_radius_in_pixel = math.sqrt((neck_bottom_left_x - neck_bottom_right_x) * (neck_bottom_left_x - neck_bottom_right_x) + (neck_bottom_left_y - neck_bottom_right_y) * (neck_bottom_left_y - neck_bottom_right_y))
+neck_side_radius_in_pixel = math.sqrt((neck_bottom_left_x - neck_bottom_right_x) * (neck_bottom_left_x - neck_bottom_right_x) + (neck_bottom_left_y - neck_bottom_right_y) * (neck_bottom_left_y - neck_bottom_right_y)) / 2
 neck_side_radius_in_cm = neck_side_radius_in_pixel * left_image_pixel_size
 
-neck_circumference = 2 * (neck_frontal_radius_in_cm + neck_side_radius_in_cm)
-print("Done!")
+neck_circumference = math.pi * (3.33 * (neck_side_radius_in_cm + neck_frontal_radius_in_cm) - math.sqrt((3.33 * neck_side_radius_in_cm + neck_frontal_radius_in_cm) * (neck_side_radius_in_cm + 3.33 * neck_frontal_radius_in_cm)))
+print(f"({int(neck_circumference)}cm) Done!")
 
 print("Calculating abdomen circumference...", flush=True, end=' ')
-abdomen_circumference = 84
-print("Done!")
+image = cv2.imread('input/Left.jpg')
+image_height, image_width, _ = image.shape
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=True)
+results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+landmarks = results.pose_landmarks.landmark
+
+left_shoulder_y = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height
+left_shoulder_x = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].x * image_width
+
+left_hip_y = landmarks[mp_pose.PoseLandmark.LEFT_HIP].y * image_height
+left_hip_x = landmarks[mp_pose.PoseLandmark.LEFT_HIP].x * image_width
+
+abdomen_point_y = int((left_shoulder_y + left_hip_y) / 2)
+
+start_point_x = 0
+end_point_x = 0
+
+for x in range(image.shape[1]):
+    if left_image_segments[abdomen_point_y][x] == 2:
+        start_point_x = x
+        break
+
+for x in range(image.shape[1] - 1, -1, -1):
+    if left_image_segments[abdomen_point_y][x] == 2:
+        end_point_x = x
+        break
+
+abdomen_side_radius_in_pixel = (end_point_x - start_point_x) / 2
+abdomen_side_radius_in_cm = abdomen_side_radius_in_pixel * left_image_pixel_size
+
+image = cv2.imread('input/Front.jpg')
+image_height, image_width, _ = image.shape
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=True)
+results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+landmarks = results.pose_landmarks.landmark
+
+left_shoulder_y = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].y * image_height
+left_shoulder_x = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER].x * image_width
+
+left_hip_y = landmarks[mp_pose.PoseLandmark.LEFT_HIP].y * image_height
+left_hip_x = landmarks[mp_pose.PoseLandmark.LEFT_HIP].x * image_width
+
+abdomen_point_y = int((left_shoulder_y + left_hip_y) / 2)
+
+start_point_x = 0
+end_point_x = 0
+
+for x in range(image.shape[1]):
+    if frontal_image_segments[abdomen_point_y][x] == 2:
+        start_point_x = x
+        break
+
+for x in range(image.shape[1] - 1, -1, -1):
+    if frontal_image_segments[abdomen_point_y][x] == 2:
+        end_point_x = x
+        break
+
+abdomen_front_radius_in_pixel = (end_point_x - start_point_x) / 2
+abdomen_front_radius_in_cm = abdomen_front_radius_in_pixel * frontal_image_pixel_size[0]
+
+abdomen_circumference = math.pi * (3.33 * (abdomen_front_radius_in_cm + abdomen_side_radius_in_cm) - math.sqrt((3.33 * abdomen_front_radius_in_cm + abdomen_side_radius_in_cm) * (abdomen_front_radius_in_cm + 3.33 * abdomen_side_radius_in_cm)))
+
+print(f"({int(abdomen_circumference)}cm) Done!")
 
 print("Predicting the body fat percentage...", flush=True, end=' ')
 bodyFatPercentage = ParametricBodyFatEstimation.parametricBodyFatEstimation(SEX, HEIGHT / 100, neck_circumference, abdomen_circumference)
 print("Done!")
 
-print('Body fat percentage:', bodyFatPercentage)
+print(f'Body fat percentage of this person is {int(bodyFatPercentage)}%')
